700de4fb4112c921c37a4fffb898fb38a1a8f2ca
sanii-muthui <muthuisanii@gmail.com>
2019-09-17 15:27:26 +0300

    added Business and Post Forms

--- hoodwatch/forms.py
+++ hoodwatch/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import *
 
-
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
