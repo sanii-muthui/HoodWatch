d23a88477e015d93431f0a49b99e426b523373b5
sanii-muthui <muthuisanii@gmail.com>
2019-09-17 15:28:40 +0300

    added signal.py

--- hoodwatch/signals.py
+++ hoodwatch/signals.py
@@ -9,6 +9,7 @@ def create_profile(sender,instance,created,**kwargs):
     if created:
         Profile.objects.create(profile=instance)
 
+
 @receiver(post_save, sender=User)
 def save_profile(sender,instance,**kwargs):
     instance.profile.save()
