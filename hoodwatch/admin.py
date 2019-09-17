d114b74752104550dbf676633a45317fea1b12be
sanii-muthui <muthuisanii@gmail.com>
2019-09-17 15:25:58 +0300

    imported profile,business,post models

--- hoodwatch/admin.py
+++ hoodwatch/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import Profile,Neighbourhood,Business,Review,Post,Comment
 
 # Register your models here.
 admin.site.register(Profile)
